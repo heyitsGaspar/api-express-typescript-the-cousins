fa6cea9615c6d1266616c67a3ea53c86e6275637
heyitsGaspar <israel.chay.32@gmail.com>
2025-06-12 13:24:44 -0600

    feat: add route to get product by id

--- src/interfaces/routes/products.routes.ts
+++ src/interfaces/routes/products.routes.ts
@@ -1,9 +1,10 @@
 import { Router } from 'express';
-import { getProductsController, postProductController } from '../controllers/products/product.controller';
+import { getProductsController, postProductController , getProductByIdController } from '../controllers/products/product.controller';
 
 const productRouter = Router();
 productRouter.get('/', getProductsController);
 productRouter.post('/', postProductController);
+productRouter.get('/:id', getProductByIdController); // Assuming you want to get a product by ID as well
 
 
 
