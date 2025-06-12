faed60d3d66047f5ea5d9eac517d0f7ee3e4b2c7
heyitsGaspar <israel.chay.32@gmail.com>
2025-06-12 13:22:07 -0600

    feat: add controller to get product by id

--- src/interfaces/controllers/products/product.controller.ts
+++ src/interfaces/controllers/products/product.controller.ts
@@ -2,6 +2,7 @@ import { Request, Response, NextFunction } from 'express';
 import { GetAllProductUseCase } from '../../../application/use-cases/products/getAllProductUseCase';
 import { ProductSupabaseRepository } from '../../../infraestructure/repositories/products/productSupabaseRepository';
 import { CreateProduct } from '../../../application/use-cases/products/createProduct';
+import { GetProductById } from '../../../application/use-cases/products/getProductById';
 
 
 const productRepository = new ProductSupabaseRepository();
@@ -25,3 +26,17 @@ export const postProductController = async (req: Request, res: Response, next: N
         res.status(error.statusCode || 500).json({ message: error.message });
     }
 }; 
+
+export const getProductByIdController = async (req: Request, res: Response, next: NextFunction) => {
+    try {
+        const { id } = req.params;
+        const useCase = new GetProductById(productRepository);
+        const product = await useCase.execute(id);
+        if (!product) {
+            return res.status(404).json({ message: 'Product not found' });
+        }
+        res.json(product);
+    } catch (error: any) {
+        res.status(error.statusCode || 500).json({ message: error.message });
+    }
+};
